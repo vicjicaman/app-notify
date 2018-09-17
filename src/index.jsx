d62979c25fa2e865d4b586d687b7d8f4829e8fc3
Victor Jimenez <vic.jicama@gmail.com>
2018-09-17 11:06:53 -0500

    Add instance commit operation

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
-import {notify, NotifyContainer} from './Notify.jsx';
+import {notify, NotifyContainer, close} from './Notify.jsx';
 import {NotifyModal} from './NotifyModal.jsx';
 
 export {
+  close,
   notify,
   NotifyContainer,
   NotifyModal
